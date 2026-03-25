bb0773116031dfc30c5e0d6551b2352ca2351239
shanzesajid <shanzesajid14@gmail.com>
2026-03-25 13:40:55 -0400

    Update main_algorithm.py

--- src/main_algorithm.py
+++ src/main_algorithm.py
@@ -1,23 +1,38 @@
 import numpy as np
 
-# main algorithm for adaptive filter
-def nlms(y, d, N, mu, delta):
-    w = np.zeros(N)
-    errors = []
 
-    common_len = min(len(y), len(d))
-    for i in range(common_len):
-        start = max(0, i - N + 1)
-        end = i + 1
-        buffer = y[start:end][::-1]
+def nlms(x, d, filter_size, step_size, regularization):
+    """
+    x = far-end reference signal
+    d = microphone signal
+    """
 
-        n_zeros = N - len(buffer)
-        if n_zeros > 0:
-            buffer = np.concatenate([buffer, np.zeros(n_zeros)])
-        
-        y_echo = np.dot(w, buffer)
-        error = d[i] - y_echo
-        errors.append(error)
-        w += (mu * error * buffer)/(np.dot(buffer,buffer) + delta)
+    x = np.asarray(x, dtype=np.float64)
+    d = np.asarray(d, dtype=np.float64)
+
+    length = min(len(x), len(d))
+    x = x[:length]
+    d = d[:length]
+
+    weights = np.zeros(filter_size, dtype=np.float64)
+    error_signal = np.zeros(length, dtype=np.float64)
+    estimated_echo = np.zeros(length, dtype=np.float64)
+
+    for n in range(length):
+        x_vec = np.zeros(filter_size, dtype=np.float64)
+
+        start = max(0, n - filter_size + 1)
+        current_samples = x[start:n + 1][::-1]
+        x_vec[:len(current_samples)] = current_samples
+
+        y_hat = np.dot(weights, x_vec)
+        error = d[n] - y_hat
+
+        estimated_echo[n] = y_hat
+        error_signal[n] = error
+
+        norm = np.dot(x_vec, x_vec) + regularization
+        weights = weights + (step_size / norm) * error * x_vec
+
+    return weights, estimated_echo, error_signal
     
-    return w, np.array(errors)
