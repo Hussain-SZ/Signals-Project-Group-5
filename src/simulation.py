833714706d897b70156437394e4af7ff8da9c993
Syed Daniyal <daniyalhussain296@gmail.com>
2026-04-04 15:10:47 +0500

    plotting mse convergence

--- src/simulation.py
+++ src/simulation.py
@@ -19,6 +19,7 @@ if not os.path.exists("../data/results"):
     os.makedirs("../data/results")
 
 fs, near_end_data = wavfile.read("../data/test_signals/hussain-near-end.wav")
+near_end_data = np.zeros_like(near_end_data) # just used for testing with far-end only
 _, far_end_data = wavfile.read("../data/test_signals/hooriya-far-end.wav")
 
 SAMPLE_RATE = fs
@@ -42,7 +43,7 @@ if len(near_end_data.shape) > 1:
     near_end_data = np.mean(near_end_data, axis=1)
 
 # normalization
-near_end_data = near_end_data.astype(np.float32) / np.max(np.abs(near_end_data))
+#near_end_data = near_end_data.astype(np.float32) / np.max(np.abs(near_end_data))
 
 far_end_data = far_end_data * 0.5   # The room absorbs energy, making the far-end echo quieter
 far_end_data = far_end_data.astype(np.float32) / np.max(np.abs(far_end_data))
@@ -129,7 +130,7 @@ erle_results = calculate_erle(echoed_signal[:L], error_signal, near_end_data[:L]
 plt.figure(figsize=(10, 6))
 
 # ERLE over time
-plt.subplot(2, 1, 1)
+plt.subplot(3, 1, 1)
 plt.plot(erle_results, color="green", linewidth=1.5)
 plt.title("Echo Return Loss Enhancement (ERLE)")
 plt.ylabel("ERLE (dB)")
@@ -140,13 +141,24 @@ plt.legend()
 
 # Misalignment
 # Comparing learned weights to true synthetic impulse response
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 2)
 # Using plot instead of stem for cleaner view with 512 taps
 plt.plot(impulse_response, label="True Room Impulse Response", linewidth=1.5)
 plt.plot(weights, label="Estimated Weights (NLMS)", linestyle="--")
 plt.title("System Identification")
 plt.legend()
 
+window_ms = 20
+window_size = 1024
+mse_convergence = [np.mean(error_signal[i:i+window_size] ** 2) 
+                   for i in range(0, len(error_signal) - window_size, 100)]
+plt.subplot(3, 1, 3)
+plt.plot(mse_convergence, color='blue', linewidth=2)
+plt.title("MSE Convergence")
+plt.xlabel("Window Index")
+plt.ylabel("MSE")
+plt.grid(True, alpha=0.3)
+plt.yscale('log')
 plt.tight_layout()
 plt.savefig("../data/results/performance_metrics.png")
 plt.show()
